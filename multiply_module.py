f3027ef56c881c8550cb07c4b5dc0bf9124b8b8f
Veerandra Kumar <potnuru.veerandra@gmail.com>
2026-09-21 13:14:22 +0530

    added login feature

--- multiply_module.py
+++ multiply_module.py
@@ -1,7 +1,12 @@
 from input_variables import a , b
 from secrets import api_key
+from login import user_name,pass_word
 
 def multuply(a,b):
     if api_key:
-        print("API key present")
-        return a*b
+        if user_name == "01" and pass_word =="001":
+            print("API key present and loggedin")
+            return a*b
+
+
+    
