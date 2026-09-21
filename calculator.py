b7e9c1c9e2f866ff7b175dea4226f6f54f50e21b
Veerandra Kumar <potnuru.veerandra@gmail.com>
2026-09-21 12:59:28 +0530

    initial commit

---
+++ calculator.py
@@ -0,0 +1,11 @@
+from input_variables import a ,b
+from subtract_module import subtract
+from multiply_module import multuply
+
+
+
+subtraction = subtract(a,b)
+multiplication = multuply(a,b)
+print("a - b  : " , subtraction)
+print("a * b  : " , multiplication)
+
